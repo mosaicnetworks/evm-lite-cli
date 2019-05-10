a64403029d40ccd0369c67e6d4b3b6ca94dfa985
Danu Kumanan <danu3006@gmail.com>
2019-05-10 15:25:11 +0100

    Removed dist folder

--- dist/commands/Interactive.d.ts
+++
@@ -1,3 +0,0 @@
-import * as Vorpal from 'vorpal';
-import Session from '../classes/Session';
-export default function commandInteractive(evmlc: Vorpal, session: Session): Vorpal.Command;
--- dist/commands/LogsView.d.ts
+++
@@ -1,3 +0,0 @@
-import * as Vorpal from 'vorpal';
-import Session from '../classes/Session';
-export default function commandLogsShow(evmlc: Vorpal, session: Session): Vorpal.Command;
