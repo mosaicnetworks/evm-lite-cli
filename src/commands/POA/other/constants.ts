a8a033b3735edbe68c3ae190d8c49ec415efab67
Danu Kumanan <danu3006@gmail.com>
2019-06-04 12:25:01 +0100

    prod_address

--- src/commands/POA/other/constants.ts
+++ src/commands/POA/other/constants.ts
@@ -18,7 +18,7 @@ export interface POASchema extends BaseContractSchema {
 }
 
 // PROD ADDRESS
-// export const POA_ADDRESS: string = '0xabbaabbaabbaabbaabbaabbaabbaabbaabbaabba';
+export const POA_ADDRESS: string = '0xabbaabbaabbaabbaabbaabbaabbaabbaabbaabba';
 
 // DEV ADDRESS
-export const POA_ADDRESS: string = '0xbac4c7373168ce9007e77ec8a388cad9f4b39801';
+// export const POA_ADDRESS: string = '0xbac4c7373168ce9007e77ec8a388cad9f4b39801';
