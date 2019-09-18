ace96369afa8601b936c8f6b9884d0f1ffc7ae29
Danu <danu3006@gmail.com>
2019-09-18 16:13:47 +0100

    update: Added initial test requirements and jest confi g

---
+++ __tests__/accounts-create.test.ts
@@ -0,0 +1,5 @@
+describe('accounts create', () => {
+	it('should test', () => {
+		expect(0).toBe(0);
+	});
+});
---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	roots: ['<rootDir>/__tests__'],
+	transform: {
+		'^.+\\.ts$': 'ts-jest'
+	},
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+	verbose: true
+};
